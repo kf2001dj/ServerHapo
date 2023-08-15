5b51ef243595d32f1e5849a52c510f3f63f4dc04
ThanhDong <cntt23022019@gmail.com>
2023-08-15 15:45:58 +0700

    edit typeorm

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -15,35 +15,3 @@ export class User {
   @Column()
   password: string;
 }
-
-// src/entities/Course.entity.ts
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
-@Entity()
-export class Course {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  txtname: string;
-
-  @Column()
-  image_url: string;
-}
-
-// src/entities/CourseUser.entity.ts
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
-import { User } from "./User.entity";
-import { Course } from "./Course.entity";
-
-@Entity()
-export class CourseUser {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @ManyToOne(() => User, (user) => user.courseUsers)
-  user: User;
-
-  @ManyToOne(() => Course, (course) => course.courseUsers)
-  course: Course;
-}
